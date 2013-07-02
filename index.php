5bf0ea9b97eeb4bc4ebff4897a3d295862e14c82
avik <avikpal.me@gmail.com>
2013-07-02 17:52:01 +0530

    parsing

--- index.php
+++ index.php
@@ -2,11 +2,8 @@
 // ---------- Main Code -----------------------
 include “twitterapi.php”;
 // Search for tweets about “something” and output them
-$search = queryTwitter(“http://search.twitter.com/
-search.json?q=something”); 
+$search = queryTwitter(“http://search.twitter.com/search.json?q=something”); 
+//we need to parse "something" like if we are querying for "avik pal" it should be  search.json?q=avik+pal
 foreach($search->results as $tweet) {
-echo “<strong>{$tweet->from_user_name}</strong> “ .
-“tweeted ‘{$tweet->text}’ “ .
-“on {$tweet->created_at}<br />\n”;
-}
+echo “<strong>{$tweet->from_user_name}</strong> " ."tweeted {$tweet->text} " ."on {$tweet->created_at}<br />\n;}
 ?>
